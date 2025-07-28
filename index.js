dc88f28004c89872bf5bcbd754af11435867da63
Naahvya <46841818+navya2006@users.noreply.github.com>
2025-07-28 17:07:05 +0530

    responsive sidenav

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//LOADING
+//SIDENAV
+
+function toggleMenu() {
+    const sideNav = document.getElementById("side-nav");
+    const overlay = document.getElementById("overlay");
+
+    if (sideNav.style.width === "250px") {
+      closeMenu();
+    } else {
+      sideNav.style.width = "250px";
+      overlay.classList.add("active");
+    }
+  }
+
+  function closeMenu() {
+    document.getElementById("side-nav").style.width = "0";
+    document.getElementById("overlay").classList.remove("active");
+  }
